d38fa0bd995c69a984dbd27d60a642f01a28ef89
mjungebl <mexicali@gmail.com>
2025-07-05 11:19:32 -0700

    adding artwork

--- import_show_metadata.py
+++ import_show_metadata.py
@@ -1,3 +1,35 @@
+
+"""
+Module: import_show_metadata
+This module provides functionality to import show metadata from concert folders into an SQLite database
+and then move processed folders to a designated destination. It checks for missing track titles, attempts
+to tag files if necessary, updates the database with the extracted track metadata, and logs any errors or
+mismatches encountered during processing.
+Functions:
+    import_show_folders(concert_folders: list, etree_db: SQLiteEtreeDB, config)
+        Imports and processes a list of concert folders. For each folder, it checks if track titles exist,
+        attempts to tag files when needed, updates the database with new metadata, and logs errors for any
+        unprocessable folders.
+        Parameters:
+            concert_folders (list): List containing the paths of concert folders to be processed.
+            etree_db (SQLiteEtreeDB): An instance representing the SQLite database connection.
+            config: Configuration object used during the tagging process.
+            tuple:
+                successfully_imported (list): Folders that were successfully processed and imported.
+                existing_folders (list): Folders which already contained matching track metadata.
+    move_folder_if_not_exists(source_folder: str, destination_folder: str)
+        Moves a folder from a source path to a specified destination if a folder with the same name does not
+        already exist there. If the destination does not exist, it will try to create it before moving the folder.
+        Parameters:
+            source_folder (str): The path of the folder to be moved.
+            destination_folder (str): The target directory where the folder should be moved.
+            bool: True if the folder was successfully moved; False otherwise.
+Main Execution:
+    When executed as a script, the module configures logging, loads configurations from a file, sets up
+    the SQLite database connection, processes the specified concert folders by importing show metadata,
+    and then moves both newly imported and already existing show folders to a defined destination folder.
+    The execution time is measured, and all significant actions and errors are logged.
+"""
 from time import perf_counter
 from pathlib import Path
 import os
@@ -125,12 +157,9 @@ if __name__ == "__main__":
     config_file = os.path.join(os.path.dirname(__file__),"config.toml")
     config = load_config(config_file)
     copy_success_path = r"X:\Downloads\_FTP\gdead.9999.updates_imported"
-    #matches = []
-    #unmatched = []
-    #errors = []
     sqlitepath_scrape = Path(r"E:\My Documents\GitHub\etree_tag\db\etree_scrape.db").as_posix()
     etreedb = SQLiteEtreeDB(sqlitepath_scrape)
-    dirnm = r'X:\Downloads\_FTP\gdead.9999.updates_test'
+    dirnm = r'X:\Downloads\_FTP\gdead.9999.updates'
     directorylist = [f.path.replace('\\','/') for f in os.scandir(dirnm) if f.is_dir()] 
     print(directorylist)
     imported_shows,existing_shows = import_show_folders(directorylist, etreedb, config)
